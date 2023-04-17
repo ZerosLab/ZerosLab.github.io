59b8c8e474dcaf94fb5e78c8c95f9869af4b5f47
Zero <121123841+ZerosLab@users.noreply.github.com>
2023-04-17 17:16:08 -0400

    Delete test.php

--- test.php
+++
@@ -1,4 +0,0 @@
-<?
-//file name: your_php_page.php
-echo file_get_contents('https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=FD891EC85ADCE99E41198B70370E3BA7&steamid=76561198303443035&format=json');
-?>
