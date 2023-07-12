829fc53507cf163c7187165a2af8df2fb465a8b1
Zero <121123841+ZerosLab@users.noreply.github.com>
2023-07-12 01:34:03 -0400

    Update fortune.js

--- fortune-js/fortune.js
+++ fortune-js/fortune.js
@@ -1,12 +1,8 @@
 quotepath = "friends.json";
-
 elementid = "fortune";
-
-
 $.getJSON(quotepath, function(test) {
 	console.log("ran");
 	num = test.length;
 	rand = Math.floor(Math.random() * num);
 	fort = test[rand];
-	document.getElementById(elementid).innerHTML = fort;
-});
+	document.getElementById(elementid).innerHTML = fort;});
