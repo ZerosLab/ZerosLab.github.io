197bc4e5ee89e4a77b4f032a81eebb29d0b82df4
Zero <121123841+ZerosLab@users.noreply.github.com>
2023-08-01 20:55:02 -0400

    Add files via upload

---
+++ cyberlite/comics.js
@@ -0,0 +1,22 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const pg = urlParams.get('pg');
+console.log(pg);
+
+
+
+
+
+if (pg == undefined){
+console.log(pg)
+		}
+else{
+		 document.getElementById("comic").innerHTML = "<img src='img/" + pg + ".png'>";
+$.getJSON('descs.json', function(hih) {  
+document.getElementById("desc").innerHTML = (hih[pg]);
+});
+					
+	};
+
+
+
