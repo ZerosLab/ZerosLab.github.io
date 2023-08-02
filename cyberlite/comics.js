8f5566a66cd7df00dbb5efe65da7d7005ef9dc09
Zero <121123841+ZerosLab@users.noreply.github.com>
2023-08-02 01:23:46 -0400

    Update comics.js

--- cyberlite/comics.js
+++ cyberlite/comics.js
@@ -36,23 +36,24 @@ const pg = urlParams.get('pg');
 window.location.href = ("https://zeroslab.github.io/cyberlite/?pg=" + (Object.values(hih).length).toString());
 	
 });}
-
-
-
-if (pg == undefined){
 	$.getJSON('descs.json', function(hih) { 
-window.location.href = ("https://zeroslab.github.io/cyberlite/?pg=" + (Object.values(hih).length).toString());
-});
-		}
+if (pg == undefined){
+	firs();
+else if (pg <= 0){
+	window.location.href = "https://zeroslab.github.io/cyberlite/?pg=1";
+}
+else if (pg > (Object.values(hih).length)){
+	lat();
+}
 else{
-		 document.getElementById("comic").innerHTML = "<img src='img/" + pg + ".png'>";
-$.getJSON('descs.json', function(hih) {  
-	console.log("ok");
-document.getElementById("desc").innerHTML = (hih[pg]);
-	console.log(hih.pg);
-	console.log(hih[pg]);
+	document.getElementById("comic").innerHTML = "<img src='img/" + pg + ".png'>";
+	$.getJSON('descs.json', function(hih) {  
+		console.log("ok");
+		document.getElementById("desc").innerHTML = (hih[pg]);
+		console.log(hih.pg);
+		console.log(hih[pg]);
 });
 					
 	};
-
+});}
 
