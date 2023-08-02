67c2648bec74bb4fd8b2a2b45b79f42413569b50
Zero <121123841+ZerosLab@users.noreply.github.com>
2023-08-01 22:22:50 -0400

    Update comics.js

--- cyberlite/comics.js
+++ cyberlite/comics.js
@@ -14,6 +14,7 @@ else{
 		 document.getElementById("comic").innerHTML = "<img src='img/" + pg + ".png'>";
 $.getJSON('descs.json', function(hih) {  
 document.getElementById("desc").innerHTML = (hih[pg]);
+	console.log(hih[pg]);
 });
 					
 	};
