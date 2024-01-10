17367001d1e97eaddc5688a8faa44191af25683d
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 17:04:31 -0500

    Add files via upload

---
+++ fangameDex/main.js
@@ -0,0 +1,24 @@
+dexJSONURL=""
+
+
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const game = urlParams.get('game');
+const type = urlParams.get('type');
+const level = urlParams.get('level');
+i = 0;
+
+
+
+for element in game.json{
+  if element.name in 
+
+}
+
+
+
+
+
+
+document.getElementById("p1").innerHTML = newtable;
