17b90fa350bad7c7e615be6d9b11ee5da12f8b33
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 17:15:21 -0500

    Update main.js

--- fangameDex/main.js
+++ fangameDex/main.js
@@ -1,6 +1,6 @@
 dexJSONURL=""
 
-
+newtable="<table><tr>"
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -8,13 +8,22 @@ const game = urlParams.get('game');
 const type = urlParams.get('type');
 const level = urlParams.get('level');
 i = 0;
+$.getJSON(dexJSONURL, function(dex)){
 
 
-
-for element in game.json{
-  if element.name in 
-
-}
+  for element in game.json{
+    i+=1;
+    newtable += "<td";
+    if element.name in dexJSON:
+      newtable +=' style="background-color:yellow"> ';
+    else:
+      newtable +=">";
+      newtable += "</td>"
+    if i%6=0:
+      newtable+="</tr><tr>"
+    if i%30 = 0:
+      newtable += "<tr><tr><td colspan=6> " + i.toString + "-" + (i+30).tostring + "</tr><tr>"
+  }
 
 
 
